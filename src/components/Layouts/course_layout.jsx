50111c37244009995396d53a54f28a34acf4afe6
Vishal Lade <83055827+lade6501@users.noreply.github.com>
2023-06-17 15:01:47 +0530

    Getting data from DB
    Calling a course API's to get all courses from mongodb database.

--- src/components/Layouts/course_layout.jsx
+++ src/components/Layouts/course_layout.jsx
@@ -1,38 +1,21 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import Course from "../Course/Course";
 import { Container, Row, Col } from "react-bootstrap";
-
-const courses = [
-  {
-    id: 1,
-    image:
-      "https://cdn.freebiesupply.com/logos/large/2x/react-1-logo-png-transparent.png",
-    courseName: "React",
-    rating: "4.5 ",
-  },
-  {
-    id: 2,
-    image: "https://openclipart.org/image/2400px/svg_to_png/272339/angular.png",
-    courseName: "Anguler",
-    rating: "4.2 ",
-  },
-  {
-    id: 3,
-    image:
-      "https://th.bing.com/th/id/OIP.CbL7Xeq2c29sTx9d-HTbXAHaHa?pid=ImgDet&rs=1",
-    courseName: "Python",
-    rating: "4.78 ",
-  },
-  {
-    id: 4,
-    image:
-      "https://th.bing.com/th/id/OIP.HR0bVR9uwNQC8m9TTGLy5AHaEK?pid=ImgDet&rs=1",
-    courseName: "Azure",
-    rating: "4.87 ",
-  },
-];
+import axios from 'axios';
 
 const course_layout = () => {
+  const [courses,setCourses] = useState([]);
+  
+  useEffect(()=>{
+    getCourses();
+  },[])
+
+  //Calling course api to get all the courses from DB
+  const getCourses = async() =>{
+    const response = await axios.get('http://localhost:8000/courses/allCourses/');
+    setCourses(response.data)
+  }
+  
   return (
     <>
       <Container>
